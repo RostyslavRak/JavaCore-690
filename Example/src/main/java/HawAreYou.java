4d5888b5e3ab0ae5d9f43d273b845af59b6de044
Rostyslav Rak <Rostyslav.rak@gmail.com>
2022-05-05 02:26:20 +0200

    HW Intro
    Home work : Introduction

---
+++ Example/src/main/java/HawAreYou.java
@@ -0,0 +1,16 @@
+
+
+
+import java.util.Scanner;
+
+public class HawAreYou {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	System.out.println("How are you?");
+	String status = sc.nextLine();
+	System.out.println("You are " + status + "!");
+
+	}
+
+}
